469da0db4206db4610168c9a70fc0e29224b89a1
Benja5178 <32654932+Benja5178@users.noreply.github.com>
2018-09-01 23:31:02 -0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,29 @@
+const Discord = require("discord.js");
+const  client = new Discord.Client();
+const config = require("./config.json");
+
+client.on("ready", () => {
+   console.log("Estoy listo!");
+   client.user.setPresence( {
+    status: "online",
+    game: {
+        name: "Prefix: Soldier  | Benja5178",
+        type: "PLAYING"
+    }
+} );
+
+});
+var prefix = config.prefix;
+
+client.on("message", (message) => {
+  if (!message.content.startsWith(config.prefix)) return;
+  if (message.author.bot) return;
+
+  if (message.content.startsWith(prefix + "ping")) {
+    message.channel.send("pong!");
+  } else
+  if (message.content.startsWith(prefix +"hola")) {
+    message.channel.send("Hola que tal?");
+  }
+});
+client.login(config.token);
